efc9e5e4711ad3e70a2a7f0b1c0f219d061afb9e
FosterClark48 <fosterclark48@yahoo.com>
2023-03-27 19:00:32 -0700

    task 4 but added route dec/func for "/number_template/<int:n>" URL that accesses html file in templates folder using the render_template function

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""
+Task 5 - Number template
+Script that starts a Flask web application listening on 0.0.0.0, port 5000
+Route: /: display "Hello HBNB!"
+Route: /hbnb: display "HBNB"
+Route: /c/<text>: display “C ” followed by the value of the text variable
+Route: /python/<text>: display "Python " followed by val of text var
+- default text value = "is cool"
+Route: /number/<n>: display “n is a number” only if n is an integer
+Routes: /number_template/<n>: display a HTML page only if n is an integer:
+- H1 tag: “Number: n” inside the tag BODY
+Use strict_slashes=False in route decorator
+"""
+from flask import Flask, render_template
+
+
+# Create instance of Flask
+app = Flask(__name__)
+
+
+# Route decorator for '/' URL, trailing slashes = ignored
+@app.route('/', methods=['GET'], strict_slashes=False)
+def hello_hbnb():
+    """ Route func that returns gretting message """
+    return "Hello HBNB!"
+
+
+# Route decorator for '/hbnb' URL, trailing slashes = ignored
+@app.route('/hbnb', methods=['GET'], strict_slashes=False)
+def hbnb():
+    """ Route func that returns 'HBNB' """
+    return "HBNB"
+
+
+# Route decorator for /c/<text> URL, trailing slashes = ignored
+@app.route('/c/<text>', methods=['GET'], strict_slashes=False)
+def c(text):
+    """ Route func that returns 'C <text>' """
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+# Route decorator for /python/<text> URL, def text = is cool
+# trailing slashes = ignored
+@app.route('/python/', methods=['GET'], defaults={'text': 'is cool'},
+           strict_slashes=False)
+@app.route('/python/<text>', methods=['GET'], strict_slashes=False)
+def python(text):
+    """ Route func that returns 'Python <text>' """
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+# Route decorator for /number/<n> URL, used only if n is int
+# trailing slashses = ignored
+@app.route('/number/<int:n>', methods=['GET'], strict_slashes=False)
+def number(n):
+    """ Route func that returns '<int:n> is a number' """
+    return '{} is a number'.format(n)
+
+
+# Route dec for /number_template/<n> URL
+# display HTML page only if n is int
+# trailing slashes = ignored
+@app.route('/number_template/<int:n>', methods=['GET'], strict_slashes=False)
+def number_template(n):
+    """ Route fun that renders template if n is int """
+    return render_template('5-number.html', n=n)
+
+# specify host as 0.0.0.0 and port as 5000 using app.run() method
+if __name__ == '__main__':
+    """ Ensure web app is only started if script is run directly """
+    app.run(host='0.0.0.0', port=5000)
