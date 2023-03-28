96d8c2f183cf5ca5ae039c84610373e349dfa373
FosterClark48 <fosterclark48@yahoo.com>
2023-03-28 15:04:33 -0700

    added past tasks to file. added docstrings

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -28,12 +28,74 @@ from models.state import State
 
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+# Route decorator for '/' URL, trailing slashes = ignored
+@app.route('/')
+def hello_hbnb():
+    """ Route func that returns gretting message """
+    return "Hello HBNB!"
+
+
+# Route decorator for '/hbnb' URL, trailing slashes = ignored
+@app.route('/hbnb')
+def hbnb():
+    """ Route func that returns 'HBNB' """
+    return "HBNB"
+
+
+# Route decorator for /c/<text> URL, trailing slashes = ignored
+@app.route('/c/<text>')
+def c(text):
+    """ Route func that returns 'C <text>' """
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+# Route decorator for /python/<text> URL, def text = is cool
+# trailing slashes = ignored
+@app.route('/python/')
+@app.route('/python/<text>')
+def python(text="is cool"):
+    """ Route func that returns 'Python <text>' """
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+# Route decorator for /number/<n> URL, used only if n is int
+# trailing slashses = ignored
+@app.route('/number/<int:n>')
+def number(n):
+    """ Route func that returns '<int:n> is a number' """
+    return '{} is a number'.format(n)
+
+
+# Route dec for /number_template/<n> URL
+# display HTML page only if n is int
+# trailing slashes = ignored
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    """ Route fun that renders template if n is int """
+    return render_template('5-number.html', n=n)
+
+
+# Route dec for /number_odd_or_even/<int:> URl
+# display HTML page only if n is int or even/odd
+# trailing slashes = ignored
+@app.route('/number_odd_or_even/<int:n>')
+def number_odd_or_even(n):
+    """ Route func that renders template if n is int """
+    if n % 2 == 0:
+        odd_or_even = "even"
+    else:
+        odd_or_even = "odd"
+    return render_template('6-number_odd_or_even.html', n=n,
+                           odd_or_even=odd_or_even)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """Display HTML page w/ list of all states present in DBStorage"""
-    states = storage.all(State).values()
+    states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
 
@@ -42,5 +104,7 @@ def teardown(exception):
     """ Removes the current SQLAlchemy Session """
     storage.close()
 
+# specify host as 0.0.0.0 and port as 5000 using app.run() method
 if __name__ == '__main__':
+    """ Ensure web app is only started if script is run directly """
     app.run(host='0.0.0.0', port=5000)
