99de1e6ca686492a617df5197e7b58eb150cf34c
FosterClark48 <fosterclark48@yahoo.com>
2023-03-28 20:48:37 -0700

    had to change it from the task 7 i made the new file from

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -6,10 +6,12 @@ from models import storage and storage.all(...)
 After each request, remove current SQLAlchemy Session:
     Declare method to handle @app.teardown_appcontext
     Call in this method: storage.close()
-Route /states_list: display a HTML page
+Route /cities_by_states: display a HTML page
     H1 tag: "States"
     UL tag: list of all State objects present in DBStorage sorted by name
         LI tag: Description of one State: <state.id>: <<B><state.name></B>
+        UL tag: list of City objects linked to the State, sorted by name
+            LI tag: description of one City: <city.id>L <B><city.name></B>
 Must use option strict_slashes=False
 """
 from flask import Flask, render_template
@@ -21,21 +23,21 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
+@app.route("/cities_by_states")
+def city_by_list():
+    """
+    Displays an HTML formatted list of cities in states from DBStorage
+    """
+    states = storage.all(State)
+    return render_template("8-cities_by_states.html", state_list=states)
+
+
 @app.teardown_appcontext
 def teardown(stuff):
     """
     Remove current SQLAlchemy session
     """
     storage.close()
-
-
-@app.route("/states_list")
-def state_list():
-    """
-    Displays an HTML formatted list of states from DBStorage
-    """
-    states = storage.all(State)
-    return render_template("7-states_list.html", state_list=states)
 
 
 if __name__ == "__main__":
